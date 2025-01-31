417edb1865e6d36d97d1ea4a64ce200c613d0496
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-01-31 22:59:27 +0200

    Update script.js

--- UX1-Query/assets/js/home/script.js
+++ UX1-Query/assets/js/home/script.js
@@ -1,1 +1,12 @@
+var home = {
+  myFunction: () => {
+    console.log("Welcome to Home!");
+  },
+  
+  init: () => {
+    document.querySelector("h1").addEventListener("click", home.myFunction);
+  }
+};
 
+// Initialize the page
+home.init();
