3668f0b765624cfa37b23a332d4385f1eb80c29a
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-01-31 21:08:09 +0200

    Update script.js

--- UX1-Query/script.js
+++ UX1-Query/script.js
@@ -19,49 +19,162 @@ subNavTriggers.forEach(trigger => trigger.addEventListener('mouseout', handleSub
 accordionTriggers.forEach(trigger => trigger.addEventListener('click', handleAccordionTrigger));
 alertCloseButtons.forEach(button => button.addEventListener('click', handleAlertClose));
 
-// Get the main content container
-const mainContentContainer = document.getElementById('main-content');
-
-// Functions
-
-const routes = {
-  'home' : 'home.html' , 
-  'pricing' : 'pricing.html' ,  
-  'about' : 'about.html' ,  
-  'contact' : 'contact.html' ,  
-  'installation' : 'installation.html' ,  
-  'upgrades' : 'upgrades.html' ,  
-  'configuration' : 'configuration.html' ,  
-  'tutorial-1' : 'tutorial-1.html' ,  
-  'tutorial-2' : 'tutorial-2.html' ,  
-  'tutorial-3' : 'tutorial-3.html' ,  
-  'api-reference' : 'api-reference.html' ,  
+function clearSections() {
+  document.querySelectorAll('section').forEach((section) => {
+    section.innerHTML = '';
+  });
 }
 
-// Function to load the main content dynamically
-function loadMainContent(url) {
-    fetch('pages/' + url)
-        .then(response => response.text())
-        .then(html => {
-            mainContentContainer.innerHTML = html;
-        })
-        .catch(error => console.error('Error loading main content:', error));
+function addSectionId(css, sectionId) {
+  var rules = css.split('}');
+  var modifiedRules = [] ;
+
+  rules.forEach(function(rule) {
+    var selectorMatch = rule.match(/([^{]+)/);
+    if (selectorMatch) {
+      var selector = selectorMatch[1].trim();
+      var modifiedSelector = `
+`     + `#${sectionId} `;
+
+      modifiedRules.push(modifiedSelector + rule.substring(selector.length) + '}');
+    }
+  });
+
+  return modifiedRules.join('').replace('body' , '');
+}
+function addSectionIdToJs(jsCode, sectionId) {
+  // Use regular expressions to find and modify query selectors
+  return   jsCode.replace(/(document\.querySelector|document\.querySelectorAll|jQuery|[$])\s*\(\s*["'](#|\.|)([a-zA-Z0-9_-]+)["']\s*\)/g, (match, p1, p2, p3) => {
+    return `${p1}('#${sectionId} ${p2}${p3}')`;
+  });
 }
 
-// Function to get the query parameter value
-function getQueryParameter(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
+
+function loadPage(pageUrl) {
+  clearSections();
+
+  fetch('pages/' + pageUrl)
+.then((response) => response.text())
+.then((html) => {
+    // Select all section elements
+       const sections = document.querySelectorAll('section'); 
+    // Loop through each section
+       window["dscript"] = window["dscript"] || [];
+
+       window["dscript"].forEach((s)=>
+       {
+          s.remove();
+       });
+  
+       sections.forEach(section => 
+       {
+            // Select all style and script elements within the section
+               const stylesAndScripts = section.querySelectorAll('style, script');
+      
+            // Remove each style and script element
+               stylesAndScripts.forEach(element => element.remove());
+        });
+  
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const links = doc.querySelectorAll('link');
+    const styles = doc.querySelectorAll('style');
+    const scripts = doc.querySelectorAll('script');
+    const pageContent = doc.body.innerHTML
+        .replace(/<script>.*?<\/script>/g, '') // Remove script tags
+        .replace(/<style>.*?<\/style>/g, '') // Remove style tags
+        .replace(/<link.*?rel="stylesheet".*?>/g, ''); // Remove CSS links
+
+    // Load page-specific CSS, HTML, and JS
+    const pageName = pageUrl.replace('.html', '');
+    const sectionId = `${pageName}`;
+    const section = document.getElementById(sectionId);
+
+    // Add CSS
+    styles.forEach(style =>{
+       const htm = style.innerHTML; 
+       if(htm){
+         const css = htm;
+         const modifiedCss = addSectionId(css.trim(), sectionId);
+         const newStyle = document.createElement('style');
+         newStyle.textContent = modifiedCss;
+         section.appendChild(newStyle);
+       }
+    });
+    links.forEach(link => {
+      if (link.getAttribute('rel') === 'stylesheet' && link.getAttribute('href').endsWith('.css')) {
+        const href = link.getAttribute('href').replace('../', '');
+        if(href){
+          fetch(href)
+          .then(response => response.text())
+          .then(css =>
+          {
+              const modifiedCss = addSectionId(css.trim(), sectionId);
+              const newStyle = document.createElement('style');
+              newStyle.textContent = modifiedCss;
+              section.appendChild(newStyle);
+          })
+          .catch(error => console.error(`Error loading CSS: ${error}`));
+          
+        }
+      }
+    });
+
+    // Add HTML
+    const contentDiv = document.createElement('div');
+    contentDiv.innerHTML = pageContent;
+  
+    const stylesAndScripts2 = contentDiv.querySelectorAll('style, script'); 
+    stylesAndScripts2.forEach(element => element.remove());
+    
+     section.appendChild(contentDiv);
+
+    // Add JS
+    scripts.forEach(script => {
+      let src = script.getAttribute('src');
+      const htm = script.innerHTML;
+      
+      if(htm){
+        const jsCode = htm;
+        const modifiedJsCode = addSectionIdToJs(jsCode, sectionId); 
+        const modifiedScript = document.createElement('script');
+        modifiedScript.textContent = modifiedJsCode;
+        section.appendChild(modifiedScript);
+        window["dscript"].push(modifiedScript);
+      }
+      else if(src)
+      { 
+          src = src.replace('../', ''); 
+        
+          fetch(src)
+          .then(response => response.text())
+          .then(jsCode => {
+              const modifiedJsCode = addSectionIdToJs(jsCode, sectionId); 
+              const modifiedScript = document.createElement('script');
+              modifiedScript.textContent = modifiedJsCode;
+              section.appendChild(modifiedScript);
+              window["dscript"].push(modifiedScript);
+            })
+          .catch(error => console.error(`Error loading JS: ${error}`));
+          }
+      
+    });
+  })
+.catch((error) => console.error(error));
 }
 
-// Load the page dynamically based on the query parameter
-const page = getQueryParameter('page');
-if (page && routes[page]) {
-    loadMainContent(routes[page]);
-} else {
-    // Load the default page if no query parameter is provided
-    loadMainContent('home.html');
-}
+const menuLinks = document.querySelectorAll('#menu a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const pageUrl = link.getAttribute('data-page');
+    loadPage(pageUrl);
+  });
+});
+
+// Load home page by default
+loadPage('home.html');
 
 function handleNavLinkClick(event) {
   event.preventDefault();
