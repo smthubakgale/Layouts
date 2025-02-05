a04e8f2de919021504fe3ed2395acbd9230c52e9
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-05 20:50:22 +0200

    Update script.js

--- UX2-Query/assets/js/logout/script.js
+++ UX2-Query/assets/js/logout/script.js
@@ -1,8 +1,17 @@
-// Get the login again button
-const loginAgainButton = document.getElementById('login-again-btn');
+var logout = { 
+  init: () => {
+   // Get the login again button
+    const loginAgainButton = document.getElementById('login-again-btn');
+    
+    // Add event listener to the login again button
+    loginAgainButton.addEventListener('click', () => {
+        // Redirect to the login page
+        window.location.href = 'login.html';
+    });    
+  }
+};
 
-// Add event listener to the login again button
-loginAgainButton.addEventListener('click', () => {
-    // Redirect to the login page
-    window.location.href = 'login.html';
-});
+// Initialize the page
+logout.init();
+
+    
